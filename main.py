69a898322a32c0d95e8cf36c786d0696ef56ff10
AlexisGR117 <jefer12543@gmail.com>
2024-01-30 12:23:34 -0500

    Función play_against e iteraciones

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import random
 
-class Person:
 
+class Person:
     names = ["Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Henry", "Ivy", "Jack"]
 
     def __init__(self):
         self.name = random.choice(self.names)
-        self.age = random.randint(18,100)
-        self.skill_level = random.randint(1,10)
-        self.winning_count = random.randint(0,20)
+        self.age = random.randint(18, 100)
+        self.skill_level = random.randint(1, 10)
+        self.winning_count = random.randint(0, 20)
 
     def get_name(self):
         return self.name
@@ -33,3 +33,35 @@ class Person:
 
     def set_winning_count(self, winning_count):
         self.winning_count = winning_count
+
+    def add_winning(self):
+        self.winning_count += 1
+
+    def play_against(self, person):
+        print(f"{self.name} (W:{self.winning_count}/SL:{self.skill_level}) vs {person.name} (W:{person.winning_count}" +
+              f"/SL:{person.skill_level})")
+        skill_level_difference = abs(self.skill_level - person.skill_level)
+        probability = 100
+        if skill_level_difference <= 2:
+            probability = 50
+        elif skill_level_difference <= 4:
+            probability = 75
+        higher_skill_level = person
+        lower_skill_level = self
+        if person.skill_level < self.skill_level:
+            higher_skill_level = self
+            lower_skill_level = person
+        winner = higher_skill_level if random.randint(1, 100) < probability else lower_skill_level
+        winner.add_winning()
+        print(f"Winner: {winner.get_name()}")
+        print(f"Result {self.get_name()} (W:{self.winning_count}) - {person.get_name()} (W:{person.winning_count})")
+
+
+people = [Person() for p in range(10)]
+
+for x in range(20):
+    print(f"-------------Iteration {x}-------------Iteration: ")
+    for p in people:
+        opponent = random.choice(people)
+        p.play_against(opponent)
+
